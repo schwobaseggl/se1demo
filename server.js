d3166438c23d428daa81721226368f2889fddf26
veit <veit@blueshoe.de>
2020-10-25 12:40:54 +0100

    add qs logging suppress favicon request

--- server.js
+++ server.js
@@ -8,18 +8,26 @@
  */
 
 // load the http module
-var http = require('http');
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
 
 // create a listener that handels webserver requests
-var requestListener = function(request, response) {
+const requestListener = function (request, response) {
+  const parsed = url.parse(request.url);
+  const query  = querystring.parse(parsed.query);
+  console.log(query);
   response.writeHead(200, {'content-type': 'text/html; charset=utf-8}'});
   // create a html document as response
-  var doc = '<h3>Welcome to Software Engineering 1</h3>'
-            + '<p>This a <b>simple</b> example of <i>html markup</i></p>'
-            + '<p>node.js documentation: <a href="https://nodejs.org/dist/latest-v4.x/docs/api/http.html">click here</a></p>'
-            + '<p>html reference: <a href="https://selfhtml.org" target="_blank">click here</a></p>'
+  let doc = '<head><link rel="icon" href="data:;base64,iVBORw0KGgo="></head>' // suppress favicon request
+      + '<body>'
+      + '<h3>Welcome to Software Engineering 1</h3>'
+      + '<p>This a <b>simple</b> example of <i>html markup</i></p>'
+      + '<p>node.js documentation: <a href="https://nodejs.org/dist/latest-v12.x/docs/api/http.html">click here</a></p>'
+      + '<p>html reference: <a href="https://selfhtml.org" target="_blank">click here</a></p>'
+      + '</body>';
   response.end(doc);
-}
+};
 
 // create and start the server
 http.createServer(requestListener).listen(8080, 'localhost');
